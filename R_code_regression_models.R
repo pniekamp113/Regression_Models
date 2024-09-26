70f6692a0cd18b3ff81f45e7d40753c66fea59d5
Patrick Niekamp <pniekamp113@gmail.com>
2024-09-26 15:59:42 -0400

    new

---
+++ R_code_regression_models.R
@@ -0,0 +1,44 @@
+#Regression Models
+
+library(UsingR)
+library(reshape)
+library(ggplot2)
+library(manipulate)
+library(dplyr)
+
+
+#Module 1
+
+myHist <- function(mu) {
+  mse <- mean((galton$child - mu)^2)
+  g <- ggplot(galton, aes(x = child)) + geom_histogram(fill = "salmon", colour = "black", binwidth = 1) +
+    geom_vline(xintercept = mu, size =3) +
+    ggtitle(paste("mu = ", mu, ", MSE = ", round(mse, 2), sep= ""))
+  g
+}
+
+manipulate(myHist(mu), mu = slider(62, 74, step = 0.5))
+
+
+
+y <- galton$child - mean(galton$child)
+x <- galton$parent - mean(galton$parent)
+freqData <- as.data.frame(table(x, y))
+names(freqData) <- c("child", "parent", "freq")
+freqData$child <- as.numeric(as.character(freqData$child))
+freqData$parent <- as.numeric(as.character(freqData$parent))
+myPlot <- function(beta){
+  g <- ggplot(filter(freqData, freq > 0), aes(x = parent, y = child))
+  g <- g  + scale_size(range = c(2, 20), guide = "none" )
+  g <- g + geom_point(colour="grey50", aes(size = freq+20, show_guide = FALSE))
+  g <- g + geom_point(aes(colour=freq, size = freq))
+  g <- g + scale_colour_gradient(low = "lightblue", high="white")                     
+  g <- g + geom_abline(intercept = 0, slope = beta, size = 3)
+  mse <- mean( (y - beta * x) ^2 )
+  g <- g + ggtitle(paste("beta = ", beta, "mse = ", round(mse, 3)))
+  g
+}
+manipulate(myPlot(beta), beta = slider(0.6, 1.2, step = 0.02))
+
+
+
