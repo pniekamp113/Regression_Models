97c7d47bf7e4b61a97e129617f82f49e2c1dbf91
Patrick Niekamp <pniekamp113@gmail.com>
2024-09-27 16:44:03 -0400

    updated

--- R_code_regression_models.R
+++ R_code_regression_models.R
@@ -5,9 +5,17 @@ library(reshape)
 library(ggplot2)
 library(manipulate)
 library(dplyr)
+library(swirl)
+install_from_swirl("Regression Models")
 
 
 #Module 1
+
+#load data
+data(galton)
+head(galton)
+long <- melt(galton)
+long
 
 myHist <- function(mu) {
   mse <- mean((galton$child - mu)^2)
@@ -41,4 +49,78 @@ myPlot <- function(beta){
 manipulate(myPlot(beta), beta = slider(0.6, 1.2, step = 0.02))
 
 
+g <- ggplot(filter(freqData, freq > 0), aes(x = parent, y = child))
+g <- g  + scale_size(range = c(2, 20), guide = "none" )
+g <- g + geom_point(colour="grey50", aes(size = freq+20, show_guide = FALSE))
+g <- g + geom_point(aes(colour=freq, size = freq))
+g <- g + scale_colour_gradient(low = "lightblue", high="white")  
+g <- g + geom_smooth(method="lm", formula=y~x)
+g
 
+
+#Regression to the mean
+
+x <- rnorm(100)
+y <- rnorm(100)
+odr <- order(x)
+x[odr[100]]
+y[odr[100]]
+odr
+
+
+library(UsingR)
+data(father.son)
+y <- (father.son$sheight - mean(father.son$sheight)) / sd(father.son$sheight)
+x <- (father.son$fheight - mean(father.son$fheight)) / sd(father.son$fheight)
+rho <- cor(x, y)
+library(ggplot2)
+
+g = ggplot(data.frame(x = x, y = y), aes(x = x, y = y))
+g = g + geom_point(size = 6, colour = "black", alpha = 0.2)
+g = g + geom_point(size = 4, colour = "salmon", alpha = 0.2)
+g = g + xlim(-4, 4) + ylim(-4, 4)
+g = g + geom_abline(intercept = 0, slope = 1)
+g = g + geom_vline(xintercept = 0)
+g = g + geom_hline(yintercept = 0)
+g = g + geom_abline(intercept = 0, slope = rho, size = 2)
+g = g + geom_abline(intercept = 0, slope = 1 / rho, size = 2)
+g
+
+
+#Quiz
+
+x <- c(0.18, -1.54, 0.42, 0.95)
+w <- c(2, 1, 3, 1)
+
+mod1 <- lm(x~w)
+summary(mod1)
+
+
+x <- c(0.8, 0.47, 0.51, 0.73, 0.36, 0.58, 0.57, 0.85, 0.44, 0.42)
+y <- c(1.39, 0.72, 1.55, 0.48, 1.19, -1.59, 1.23, -0.65, 1.49, 0.05)
+
+mod <- lm(x~y)
+summary(mod)
+
+data(mtcars)
+
+mod2 <- lm(mtcars$mpg ~ mtcars$wt)
+summary(mod2)  
+
+plot(mtcars$mpg, mtcars$wt)
+
+
+
+x <- c(8.58, 10.46, 9.01, 9.64, 8.86)
+x_normalized <- (x - mean(x)) / sd(x)
+x_normalized[1]
+
+
+x <- c(0.8, 0.47, 0.51, 0.73, 0.36, 0.58, 0.57, 0.85, 0.44, 0.42)
+y <- c(1.39, 0.72, 1.55, 0.48, 1.19, -1.59, 1.23, -0.65, 1.49, 0.05)
+
+mod <- lm(y ~x)
+summary(mod)
+
+x <- c(0.8, 0.47, 0.51, 0.73, 0.36, 0.58, 0.57, 0.85, 0.44, 0.42)
+mean(x)
